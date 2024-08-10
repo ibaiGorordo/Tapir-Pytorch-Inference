6d252fa4e4a7a3049eab2c3ac11125c8b2eb2126
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-08-10 15:43:03 +0900

    Update export_onnx.py

--- export_onnx.py
+++ export_onnx.py
@@ -1,4 +1,4 @@
-import numpy as np
+import argparse
 import onnx
 import torch
 from onnxsim import simplify
@@ -6,40 +6,46 @@ from onnxsim import simplify
 from tapnet.tapir_inference import TapirPredictor, TapirPointEncoder, build_model
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-random = np.random.RandomState(2)
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="Tapir ONNX Export")
+    parser.add_argument("--model", default="models/causal_bootstapir_checkpoint.pt", type=str,
+                        help="path to Tapir checkpoint")
+    parser.add_argument("--resolution", default=640, type=int, help="Input resolution")
+    parser.add_argument("--num_points", default=1000, type=int, help="Number of points")
+    parser.add_argument("--num_iters", default=4, type=int, help="Number of iterations, 1 for faster inference, 4 for better results")
+    return parser
 
 
 if __name__ == '__main__':
-    resize_height = 256
-    resize_width = 256
-    num_points = 256
-    num_iters = 4
+    parser = get_parser()
+    args = parser.parse_args()
 
-    model = build_model('models/causal_bootstapir_checkpoint.pt', (resize_height, resize_width), num_iters, True, device)
-    predictor = TapirPredictor(model).to(device)
-    encoder = TapirPointEncoder(model).to(device)
+    resolution = args.resolution
+    num_points = args.num_points
+    num_iters = args.num_iters
+
+    model = build_model('models/causal_bootstapir_checkpoint.pt',
+                        (resolution, resolution), num_iters, True, device).eval()
+    predictor = TapirPredictor(model).to(device).eval()
+    encoder = TapirPointEncoder(model).to(device).eval()
 
     causal_state_shape = (num_iters, model.num_mixer_blocks, num_points, 2, 512 + 2048)
     causal_state = torch.zeros(causal_state_shape, dtype=torch.float32, device=device)
-    feature_grid = torch.zeros((1, 32, 32, 256), dtype=torch.float32, device=device)
-    hires_feats_grid = torch.zeros((1, 64, 64, 128), dtype=torch.float32, device=device)
+    feature_grid = torch.zeros((1, resolution//8, resolution//8, 256), dtype=torch.float32, device=device)
+    hires_feats_grid = torch.zeros((1, resolution//4, resolution//4, 128), dtype=torch.float32, device=device)
     query_points = torch.zeros((num_points, 2), dtype=torch.float32, device=device)
-    input_resolution = torch.tensor((resize_height, resize_width)).to(device)
-    input_frame = torch.zeros((1, 3, resize_height, resize_width), dtype=torch.float32, device=device)
-
+    input_resolution = torch.tensor((resolution, resolution)).to(device)
+    input_frame = torch.zeros((1, 3, resolution, resolution), dtype=torch.float32, device=device)
 
     # Test model
     query_feats, hires_query_feats = encoder(query_points[None], feature_grid, hires_feats_grid, input_resolution)
     tracks, visibles, causal_state, _, _ = predictor(input_frame, query_feats, hires_query_feats, causal_state)
 
-    print(query_points[None].shape, feature_grid.shape, hires_feats_grid.shape, input_resolution.shape)
-    print(query_feats.shape, hires_query_feats.shape)
-    print(tracks.shape, visibles.shape, causal_state.shape, feature_grid.shape, hires_feats_grid.shape)
-
     # Export model
     torch.onnx.export(encoder,
                         (query_points[None], feature_grid, hires_feats_grid, input_resolution),
-                        'tapir_encoder.onnx',
+                        'tapir_pointencoder.onnx',
                         verbose=True,
                             input_names=['query_points', 'feature_grid', 'hires_feats_grid', 'input_resolution'],
                             output_names=['query_feats', 'hires_query_feats'])
@@ -52,11 +58,11 @@ if __name__ == '__main__':
                         output_names=['tracks', 'visibles', 'causal_state', 'feature_grid', 'hires_feats_grid'])
 
     # Simplify model
-    model_simp, check = simplify('tapir_encoder.onnx')
-    onnx.save(model_simp, 'tapir_encoder_simplified.onnx')
+    model_simp, check = simplify('tapir_pointencoder.onnx')
+    onnx.save(model_simp, 'tapir_pointencoder.onnx')
 
     model_simp, check = simplify('tapir.onnx')
-    onnx.save(model_simp, 'tapir_simplified.onnx')
+    onnx.save(model_simp, 'tapir.onnx')
 
 
 
