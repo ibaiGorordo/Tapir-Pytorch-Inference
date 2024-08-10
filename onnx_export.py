1ee02f0c4a47bcfefe5e72f607b92beb20fdae18
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-08-10 16:12:45 +0900

    Update onnx_export.py

--- onnx_export.py
+++ onnx_export.py
@@ -13,6 +13,7 @@ def get_parser():
                         help="path to Tapir checkpoint")
     parser.add_argument("--resolution", default=640, type=int, help="Input resolution")
     parser.add_argument("--num_points", default=1000, type=int, help="Number of points")
+    parser.add_argument("--dynamic", action="store_true", help="Use dynamic number of points")
     parser.add_argument("--num_iters", default=4, type=int, help="Number of iterations, 1 for faster inference, 4 for better results")
     parser.add_argument("--output_dir", default="./", type=str, help="Output ONNX file")
     return parser
@@ -25,6 +26,7 @@ if __name__ == '__main__':
     model_path = args.model
     resolution = args.resolution
     num_points = args.num_points
+    dynamic = args.dynamic
     num_iters = args.num_iters
     output_dir = args.output_dir
 
@@ -44,20 +46,36 @@ if __name__ == '__main__':
     query_feats, hires_query_feats = encoder(query_points[None], feature_grid, hires_feats_grid, input_resolution)
     tracks, visibles, causal_state, _, _ = predictor(input_frame, query_feats, hires_query_feats, causal_state)
 
+    encode_dynamic = None
+    tapir_dynamic = None
+    if dynamic:
+        encode_dynamic = {'query_points': {1: 'num_points'},
+                          'query_feats': {1: 'num_points'},
+                          'hires_query_feats': {1: 'num_points'}}
+
+        tapir_dynamic = {'query_feats': {1: 'num_points'},
+                         'hires_query_feats': {1: 'num_points'},
+                         'causal_state': {2: 'num_points'},
+                         'tracks': {1: 'num_points'},
+                         'visibles': {1: 'num_points'},
+                         'new_causal_state': {2: 'num_points'}}
+
     # Export model
     torch.onnx.export(encoder,
                         (query_points[None], feature_grid, hires_feats_grid, input_resolution),
                         f'{output_dir}/tapir_pointencoder.onnx',
                         verbose=True,
-                            input_names=['query_points', 'feature_grid', 'hires_feats_grid', 'input_resolution'],
-                            output_names=['query_feats', 'hires_query_feats'])
+                        input_names=['query_points', 'feature_grid', 'hires_feats_grid', 'input_resolution'],
+                        output_names=['query_feats', 'hires_query_feats'],
+                        dynamic_axes=encode_dynamic)
 
     torch.onnx.export(predictor,
                       (input_frame, query_feats, hires_query_feats, causal_state),
                       f'{output_dir}/tapir.onnx',
                       verbose=True,
-                        input_names=['input_frame', 'query_feats', 'hires_query_feats', 'causal_state'],
-                        output_names=['tracks', 'visibles', 'causal_state', 'feature_grid', 'hires_feats_grid'])
+                      input_names=['input_frame', 'query_feats', 'hires_query_feats', 'causal_state'],
+                      output_names=['tracks', 'visibles', 'new_causal_state', 'feature_grid', 'hires_feats_grid'],
+                      dynamic_axes=tapir_dynamic)
 
     # Simplify model
     model_simp, check = simplify(f'{output_dir}/tapir_pointencoder.onnx')
